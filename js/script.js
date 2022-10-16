f7749121dc0858fee202ac297b7780ae7e35c52f
sem-a <76011034+sem-a@users.noreply.github.com>
2022-10-16 12:08:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,55 +1,56 @@
-
-let pageSlider = new Swiper('.page', {
-    // Свои классы
-    wrapperClass: 'page__wrapper',
-    slideClass: 'page__screen',
-
-    // Вертикальный слайдер
-    direction: 'vertical',
-
-    // Количество слайдов для показа 
-    slidePerView: 'auto',
-
-    // Включая параллакс
-    parallax: true,
-
-    // Управление клавиатурой
-    keyboard: {
-        // включить/выключить
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true,
-    },
-
-    // Управление колесом мыши
-    mousewheel: {
-        // Чувствительность колеса мыши
-        sensitivity: 1,
-    },
-     
-    watchOverflow: true,
-    speed: 800,
-    observer: true,
-    observeParents: true,
-    observeSlideChildren: true,
-
-
-    pagination: {
-        el: '.page__pagination',
-        type: 'bullets',
-        clickable: true,
-        bulletClass: 'page__bullet',
-        bulletActiveClass: 'page__bullet_active',
-    },
-
-    scrollbar: {
-        el: '.page__scroll',
-        dragClass: 'page__drag-scroll',
-        draggable: true,
-    },
-
-});
-
+let screenWidth = window.innerWidth;
+if (screenWidth > 0) {
+    let pageSlider = new Swiper('.page', {
+        // Свои классы
+        wrapperClass: 'page__wrapper',
+        slideClass: 'page__screen',
+    
+        // Вертикальный слайдер
+        direction: 'vertical',
+    
+        // Количество слайдов для показа 
+        slidePerView: 'auto',
+    
+        // Включая параллакс
+        parallax: true,
+    
+        // Управление клавиатурой
+        keyboard: {
+            // включить/выключить
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        },
+    
+        // Управление колесом мыши
+        mousewheel: {
+            // Чувствительность колеса мыши
+            sensitivity: 1,
+        },
+         
+        watchOverflow: true,
+        speed: 800,
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true,
+    
+    
+        pagination: {
+            el: '.page__pagination',
+            type: 'bullets',
+            clickable: true,
+            bulletClass: 'page__bullet',
+            bulletActiveClass: 'page__bullet_active',
+        },
+    
+        scrollbar: {
+            el: '.page__scroll',
+            dragClass: 'page__drag-scroll',
+            draggable: true,
+        },
+    
+    });    
+}
 
 
 // Мой скрипт
